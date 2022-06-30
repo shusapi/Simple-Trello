139c478ae284c0342691c4fb3c2bba0b5cfdd8a8
Atsushi Suzuki <atsushi@meitetsucom.co.jp>
2022-06-30 20:57:21 +0900

    all done

---
+++ src/components/task/button/TaskCardDeleteButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from "@fortawesome/free-solid-svg-icons"
+
+export const TaskCardDeleteButton = ({taskCardsList, setTaskCardsList, taskCard}) => {
+  const taskCardDeleteButton = (id) => {
+    //タスクカードを削除する
+    setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
+  };
+  return (
+    <div>
+      <button className="taskCardDeleteButton" onClick={() => taskCardDeleteButton(taskCard.id)}>
+        <FontAwesomeIcon icon={faXmark} />
+      </button>
+    </div>
+  )
+}
